891b19f019a384bcc096c3e0e4bc1b0ab2090617
iradualbert <albert.iradukunda@yahoo.com>
2021-12-12 23:31:12 +0300

    changed styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+import NewsCard from "./components/NewsCard";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [currentPage, setCurrentPage] = useState(1);
+	const [articles, setArticles] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [searchText, setSearchText] = useState("");
+	const [query, setQuery] = useState("");
+
+	const loader = useRef();
+
+	useEffect(() => {
+		if (isLoading) return;
+		const handleObserver = (entries) => {
+			const loaderEntry = entries[0];
+			if (loaderEntry.isIntersecting) {
+				console.log("You scrolled to the bottom");
+				setCurrentPage(currentPage + 1);
+			}
+		};
+		const options = {
+			root: null,
+			rootMargin: "0px",
+			threshold: 0.1,
+		};
+		const observer = new IntersectionObserver(handleObserver, options);
+		if (loader.current) {
+			observer.observe(loader.current);
+		}
+	}, [loader, isLoading]);
+
+	useEffect(() => {
+		setIsLoading(true);
+		const fetchData = async () => {
+			try {
+				const { data } = await axios.get(
+					"https://hn.algolia.com/api/v1/search?",
+					{
+						params: { page: currentPage, query },
+					}
+				);
+				const { hits } = data;
+				setArticles((previousArticles) => previousArticles.concat(hits));
+			} catch (error) {
+				console.log(error);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		fetchData();
+	}, [query, currentPage]);
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		setCurrentPage(0);
+		setQuery(searchText);
+	};
+
+	return (
+		<div className="container">
+			<h1>Hacker News</h1>
+			<form className="search-form" onSubmit={handleSubmit}>
+				<input
+					placeholder="Search for news"
+					value={searchText}
+					onChange={(e) => setSearchText(e.target.value)}
+				/>
+				<button type="submit">Search</button>
+			</form>
+			{
+				<div className="news-cards">
+					{articles.map((article) => (
+						<NewsCard article={article} key={article.objectID} />
+					))}
+					<p ref={loader}>Loading More....</p>
+				</div>
+			}
+		</div>
+	);
+};
 
 export default App;
